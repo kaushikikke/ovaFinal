ca4fe8d1481163ff371799166fdfcdc97d123c8d
kaushikikke <kaushikikke4@gmail.com>
2024-10-03 21:22:11 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import AboutUs from './components/AboutUs';
+import './AboutUsCSS.css';
+function App() {
+  return (
+    <div className="App">
+      <AboutUs />
+    </div>
+  );
+}
+
+export default App;
